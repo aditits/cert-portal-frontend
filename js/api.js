63cbb9fb8e47ce06683ca601c28876ac2885bc3a
Rahul T <rahulmanoj@cet.ac.in>
2021-06-26 22:56:30 +0530

    update

--- js/api.js
+++ js/api.js
@@ -11,7 +11,8 @@ const getdata = async (name) => {
 form.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    let name = form.name.value
+    let name = form.name.value.toLowerCase()
+    name = name.replace(" ", "")
     getdata(name).then(res => {
         console.log(res.url);
         localStorage.setItem("urls", JSON.stringify(res.url))
